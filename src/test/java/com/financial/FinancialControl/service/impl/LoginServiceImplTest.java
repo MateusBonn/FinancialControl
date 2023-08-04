6f4c771a08250b8325763033136ed66db3939696
Mateus Bonn Ferreira <mateusbonn.mbf@gmail.com>
2023-08-04 11:54:59 -0300

    Implement tests

--- src/test/java/com/financial/FinancialControl/service/impl/LoginServiceImplTest.java
+++ src/test/java/com/financial/FinancialControl/service/impl/LoginServiceImplTest.java
@@ -7,11 +7,10 @@ import com.financial.FinancialControl.repository.LoginRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-
-
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -31,19 +30,21 @@ class LoginServiceImplTest {
     @InjectMocks
     private LoginServiceImpl loginService;
 
+    SaveLoginDTO request = new SaveLoginDTO("031.260.940-07",
+            "Mateus Bonn Ferreira",
+            LocalDate.of(1993, 7, 11),
+            "mateusbonn@user.com",
+            "mbonn",
+            "senha");
+    LoginModel entity = LoginModel.builder().build();
+
 
     @Test
     void saveLogin() {
-        SaveLoginDTO request = new SaveLoginDTO("031.260.940-07",
-                                                "Mateus Bonn Ferreira",
-                                                LocalDate.of(1993, 7, 11),
-                                                "mateusbonn@user.com",
-                                                "mbonn",
-                                                "senha");
-        LoginModel entity = LoginModel.builder().build();
 
+        when(loginRepository.findByCpf(any(String.class))).thenReturn(Mono.empty());
         when(loginMapper.toEntity(any(SaveLoginDTO.class))).thenReturn(entity);
         when(loginRepository.save(any(LoginModel.class))).thenReturn(Mono.just(LoginModel.builder().build()));
-
+        assertNotNull(loginService.saveLogin(request));
     }
 }
